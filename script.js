c82816fe65579bf6ed53049a13b0915b012006cd
Anunay-Anand <anunayanand@gmail.com>
2021-10-27 16:59:39 +0530

    Added quote logic

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ let apiQuotes = [];
 
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  quoteText.textContent = quote.text;
+
+  // Check if author field is blank. Replace it with 'Unknown';
+  if (!quote.author) {
+    quote.author = "Unknown";
+  }
+  authorText.textContent = quote.author;
+
+  // Check Quote length to determine it's styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    // If such a class was added in previous instances remove it.
+    quoteText.classList.remove("long-quote");
+  }
 }
 
 // Fetching data from API with axios
@@ -18,7 +33,7 @@ async function getQuotes() {
   try {
     const res = await axios.get(apiUrl);
     // Stores the fetched Quotes Globally
-    apiQuotes.push(res.data);
+    apiQuotes = res.data;
     // Randomly selecting new Quote
     newQuote();
   } catch (e) {
